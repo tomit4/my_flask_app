47cd85fd8e31c4fd5b5ec3cd08d789519e9c7072
Brian Hayes <mosssap@gmail.com>
2021-03-21 03:18:43 -0700

    new file:   README.txt 	new file:   app.py 	new file:   data.py 	new file:   templates/about.html 	new file:   templates/article.html 	new file:   templates/articles.html 	new file:   templates/dashboard.html 	new file:   templates/data.py 	new file:   templates/home.html 	new file:   templates/includes/_formhelpers.html 	new file:   templates/includes/_messages.html 	new file:   templates/includes/_navbar.html 	new file:   templates/layout.html 	new file:   templates/login.html 	new file:   templates/register.html

---
+++ README.txt
@@ -0,0 +1,13 @@
+My First Flask App Tutorial
+
+This is my first introduction to using Flask and Python.  It is a direct step by
+step following of Brad Traversy's Python Flask from Scratch Youtube Series.
+
+This tutorial introduced me to the Flask framework and some of its syntax, as well
+as the MySQL/MariaDB program and its syntax.
+
+There was a lot of troubleshooting with this one as getting MariaDB to 'talk' to Flask
+was somewhat of a challenge, although it was also quite enlightening as to the basics
+of how front and backend programs interact.
+
+More versions to come, two videos to the tutorial are left.
---
+++ data.py
@@ -0,0 +1,25 @@
+def Articles():
+    articles = [
+        {
+            'id': 1,
+            'title':'Article One',
+            'body':'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum necessitatibus autem itaque facilis minima nesciunt dignissimos repellendus odio! Eligendi temporibus deserunt qui saepe molestiae aspernatur quas voluptate placeat earum corporis?',
+            'author':'Brian Hayes',
+            'create_date':'04-25-2017'
+        },
+        {
+            'id': 2,
+            'title':'Article Two',
+            'body':'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum necessitatibus autem itaque facilis minima nesciunt dignissimos repellendus odio! Eligendi temporibus deserunt qui saepe molestiae aspernatur quas voluptate placeat earum corporis?',
+            'author':'Brad Traversy',
+            'create_date':'04-25-2017'
+        },
+        {
+            'id': 3,
+            'title':'Article Three',
+            'body':'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum necessitatibus autem itaque facilis minima nesciunt dignissimos repellendus odio! Eligendi temporibus deserunt qui saepe molestiae aspernatur quas voluptate placeat earum corporis?',
+            'author':'John Doe',
+            'create_date':'04-25-2017'
+        },
+    ]
+    return articles
